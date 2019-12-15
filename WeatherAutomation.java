129abf6a1b78b37530b795a91e3dc59010096a58
Vitaly Gorelik <vitalygorelik1@gmail.com>
2019-12-15 17:33:33 +0200

    singleton version

--- WeatherAutomation.java
+++ WeatherAutomation.java
@@ -1,60 +1,48 @@
 package Project;
 
 import Project.Locators;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class WeatherAutomation {
+public class WeatherAutomation implements Locators {
 
 	public static void main(String[] args)  {
-		
+		fetchWeather();
 	}
 	
 	
 	public static  WebElement openChrome(WebDriver driver) {
 	//open browser with desired URL
-		driver.get(Locators.initalWebsite);
+		driver.get(initalWebsite);
 	//Maximise window
 		driver.manage().window().maximize();
 		System.out.println("opened");
 		//search for current weather
-		WebElement searchbar = (WebElement) driver.findElement(By.xpath(Locators.searchbarXPATH));
+		WebElement searchbar = (WebElement) driver.findElement(By.xpath(searchbarXPATH));
 		searchbar.click();
 		searchbar.clear();
-		searchbar.sendKeys(Locators.subject);
-		WebElement searchButton = (WebElement) driver.findElement(By.name(Locators.buttonName));
+		searchbar.sendKeys(subject);
+		WebElement searchButton = (WebElement) driver.findElement(By.name(buttonName));
 		searchButton.click();
 		return searchbar;
 	}
 	
 	public static String fetchWeather() {
-		WebDriver driver = getWebdriver();
-		WebElement search = openChrome(driver);
+		
+		WebDriver driver = WebdriverHandler.getInstance();
+		openChrome(driver);
 		WebDriverWait wait = new WebDriverWait(driver, 6);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Locators.resultId)));
-		String value = driver.findElement(By.id(Locators.resultId)).getText();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(resultId)));
+		String value = driver.findElement(By.id(resultId)).getText();
 		//Closing driver
 		driver.close();
 		
 		return "It's currently "+value+" degrees celcius";
 		
 	}
-	public static WebDriver getWebdriver() {
-		
-		//Setting the driver executable						
-		System.setProperty("webdriver.chrome.driver", "Your PATH");
-		
-		//Initiating my chrome driver
-		WebDriver driver = new ChromeDriver();
-		
-		//Applied wait time
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		
-		return driver;
+	
 	}
-}
+
--- WebdriverHandler.java
+++ WebdriverHandler.java
@@ -1,13 +1,31 @@
 package Project;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebdriverHandler {
-	private WebDriver driver;
+	//SINGELTONE
+	private static WebDriver driver;
 	
-	public WebdriverHandler() {
-		
-		
+	WebdriverHandler() {}
+
+	public static WebDriver getInstance() {
+		if (driver==null) {
+			//Initialisation 
+			driver = new ChromeDriver();
+			configDriver();
+		}
+		return driver;
+	}
+	
+	private static void configDriver() {
+		//Setting the driver executable (add your own path)						
+		System.setProperty("webdriver.chrome.driver", "Your PATH");
+						
+		//Applied wait time
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);			
 	}
 	
 }
