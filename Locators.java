9d559b2c20d711a7d4deea9d3200d4b39eb608da
Vitaly Gorelik <vitalygorelik1@gmail.com>
2019-12-15 16:43:43 +0200

    First Version

---
+++ Locators.java
@@ -0,0 +1,10 @@
+package Project;
+
+public interface Locators {
+
+	public static String initalWebsite = "https://www.google.com";
+	public static String searchbarXPATH = "//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input";
+	public static String subject = "Weather";
+	public static String buttonName = "btnK";
+	public static String resultId = "wob_tm";
+}
---
+++ WebdriverHandler.java
@@ -0,0 +1,13 @@
+package Project;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebdriverHandler {
+	private WebDriver driver;
+	
+	public WebdriverHandler() {
+		
+		
+	}
+	
+}
